51f6ce42268f05b257446c838ac23ef9f2f14f86
Данила Деренченко <daniladerencenko@Danilas-MacBook-Pro.local>
2024-06-28 17:57:04 +0400

    initual commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <button @click="requestsOnServer('PUT')">Добавить пользователя</button>
+  </template>
+
+<script>
+import axios from 'axios';
+export default {
+    methods: {
+        requestsOnServer(type) {
+            if(type == 'PUT') {
+                axios.put('http://localhost:3003/users/b74e', JSON.stringify({
+                    name: "Hello"
+                }))
+            }
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
